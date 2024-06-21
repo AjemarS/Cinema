3b7335494a6653d400b57b0f58005af4d2f4e3e4
AjemarS <sashabratashchuksasha@gmail.com>
2024-06-21 06:47:43 +0300

    updated RoomCard for search users, styled links in cards

--- cinema/src/components/Dashboard/cards/RoomCard.tsx
+++ cinema/src/components/Dashboard/cards/RoomCard.tsx
@@ -10,14 +10,18 @@ const RoomCard = ({ room }: RoomCard) => {
   // Create link for every ID (that refers to object), that will redirect to admin user page, for example
   return (
     <div className="admin-card">
-      <div>RoomId: {room.roomId}</div>
+      <div>
+        RoomId: <Link to={`/rooms/${room.roomId}`}>{room.roomId}</Link>
+      </div>
       <div>
         MovieId: <Link to={`/admin/dashboard/movies?movie=${room.movie}`}>{room.movie}</Link>
       </div>
       <div>
         Users - {room.users.length}:{" "}
         {room.users.map((user) => (
-          <li key={user}>{user}</li>
+          <li key={user}>
+            <Link to={`/admin/dashboard/users?user=${user}`}>{user}</Link>
+          </li>
         ))}
       </div>
     </div>
