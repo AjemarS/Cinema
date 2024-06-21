5a12315211773f6c217ce06a96c1d05390bf7132
AjemarS <sashabratashchuksasha@gmail.com>
2024-06-21 07:54:51 +0300

    added refresh button into dashboard

--- cinema/src/components/Navbar.tsx
+++ cinema/src/components/Navbar.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
-import SearchRoomInput from "./Cinema/Movies/SearchInput";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRotate } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar: React.FC = () => {
+  const [isUpdateMode, setIsUpdateMode] = useState(false);
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isUpdateMode) {
+      return;
+    }
+
+    const interval = setInterval(() => {
+      navigate(0);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [isUpdateMode, navigate]);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -14,8 +29,16 @@ const Navbar: React.FC = () => {
   return (
     <div className="navbar">
       <div className="navbar__logo">Dashboard</div>
-      <SearchRoomInput data={[]} />
       <div>
+        {isUpdateMode ? (
+          <button className="navbar__upd__btn" onClick={() => setIsUpdateMode(!isUpdateMode)}>
+            <FontAwesomeIcon icon={faRotate} size="lg" style={{ color: "#00ff00" }} />
+          </button>
+        ) : (
+          <button className="navbar__upd__btn" onClick={() => setIsUpdateMode(!isUpdateMode)}>
+            <FontAwesomeIcon icon={faRotate} size="lg" style={{ color: "#ff0000" }} />
+          </button>
+        )}
         {localStorage.getItem("token") ? (
           <button onClick={handleLogout} className="navbar__btn">
             Logout
